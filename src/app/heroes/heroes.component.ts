66a8dc5d9f01f7223561d9071c158af66d3e6c22
trinhtc <trinhtc@mti-tech.vn>
2018-11-22 17:22:01 +0700

    Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,6 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
   heroes: Hero[];
   constructor(private heroService: HeroService) { }
 
@@ -21,10 +20,6 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     console.log('ngOnInit');
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
 }
